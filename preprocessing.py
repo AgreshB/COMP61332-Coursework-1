7654f129fcf5475e6c8d7f0ce87c8358536159b9
Surya Prasad <techniboy.h2o@gmail.com>
2021-03-02 01:30:55 +0000

    write functions to complete the preprocessing pipeline

--- preprocessing.py
+++ preprocessing.py
@@ -47,7 +47,91 @@ def create_vocab(data):
     #creating set in order to avoid duplicates
     vocab = set()
     for x in data:
-        clean_x = process_sentence(x)
-        vocab.update(clean_x)
+        # clean_x = process_sentence(x)
+        vocab.update(x)
     return list(vocab)
+
+def create_embeddings(vocab):
+    embeddings = {}
+    vocab_embeddings = []
+    for word in vocab:
+        embed = embeddings.get(word)
+        if embed:
+            vocab_embeddings.append(embed)
+        else:
+            vocab_embeddings.append(embeddings['UNK'])
+    vocab_embeddings.append(embeddings['UNK'])
+    return vocab_embeddings
+
+def create_sentence_representation(sentences, vocab, vocab_embeddings):
+    sentence_rep = []
+    for s in sentences:
+        sr = []
+        for word in s.split():
+            if word in vocab:
+                sr.append(vocab.index(word))
+            else:
+                sr.append(len(vocab_embeddings) - 1)
+        sentence_rep.append(torch.tensor(sr))
+
+def create_labels_index_representation(labels):
+    label_indices = {}
+    label_representation = []
+    count = 0
+    for label in label:
+        if label not in label_indices:
+            label_indices[token] = count
+            count += 1
+    for label in labels:
+        label_representation.append(label_indices[label])
+    return label_indices, label_representation
+
+
+# performs necessary preprocessing and return relevant data
+def preprocess_pipeline(file_path: str):
+    labels = []
+    sentences = []
+    vocabulary = []
+    vocabulary_embed = []
+    sentence_representation = []
+    label_index = {}
+    label_representation = []
+
+    # open file (containing sentences) and read line by line
+    with open(file_path) as fp:
+        # read first line
+        line = fp.readline()
+
+        # loop to read all lines
+        while line:
+            # get current label
+            curr_label = line.split()[0]
+
+            # add label
+            labels.append(curr_label)
+
+            # clean current sentence
+            curr_sentence = split_tokenize(line)[1:]
+            curr_sentence = remove_stop(curr_sentence)
+            curr_sentence = remove_punc(curr_sentence)
+
+            # add sentence
+            sentences.append(curr_sentence)
+
+            # read next line
+            line = fp.readline()
+
+    # generate vocabulary
+    vocabulary = create_vocab(sentences)
+
+    # generate the embeddings wrt vocabulary
+    vocabulary_embed = create_embeddings(vocabulary)
+
+    # generate sentence representations using vocab
+    sentence_representation = create_sentence_representation(sentences, vocabulary, vocabulary_embed)
+
+    # generate label indices and label representation
+    label_index, label_representation = create_labels_index_representation(labels)
+
+    return labels, sentences, vocabulary, vocabulary_embed, sentence_representation, label_index, label_representation
 
