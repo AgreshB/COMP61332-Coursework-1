653b22b85a383088756994ed6898e6682e5f88b2
Surya Prasad <techniboy.h2o@gmail.com>
2021-03-02 03:17:05 +0000

    fix punctuation bug in preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,14 +1,25 @@
 import torch
 from torch import nn
-from collections import defaultdict
-import time
-import matplotlib.pyplot as plt
+# from collections import defaultdict
+# import time
+# import matplotlib.pyplot as plt
+import numpy as np
+from string import punctuation
+import re
 
 # creating list of stop words from Stop_words.txt
-STOP_WORDS = []
-with open("res/stop_words.txt") as f:
-    for line in f:
-        STOP_WORDS.append(line.strip())
+# STOP_WORDS = []
+# with open("res/stop_words.txt") as f:
+#     for line in f:
+#         STOP_WORDS.append(line.strip())
+
+# techni is testing HEHEHE
+NLTK_STOP_WORDS = set(['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"])
+MORE_STOP_WORDS = set(['a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', "aren't", 'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by', "can't", 'cannot', 'could', "couldn't", 'did', "didn't", 'do', 'does', "doesn't", 'doing', "don't", 'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had', "hadn't", 'has', "hasn't", 'have', "haven't", 'having', 'he', "he'd", "he'll", "he's", 'her', 'here', "here's", 'hers', 'herself', 'him', 'himself', 'his', 'how', "how's", 'i', "i'd", "i'll", "i'm", "i've", 'if', 'in', 'into', 'is', "isn't", 'it', "it's", 'its', 'itself', "let's", 'me', 'more', 'most', "mustn't", 'my', 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours', 'ourselves', 'out', 'over', 'own', 'same', "shan't", 'she', "she'd", "she'll", "she's", 'should', "shouldn't", 'so', 'some', 'such', 'than', 'that', "that's", 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there', "there's", 'these', 'they', "they'd", "they'll", "they're", "they've", 'this', 'those', 'through', 'to', 'too', 'under', 'until', 'up', 'very', 'was', "wasn't", 'we', "we'd", "we'll", "we're", "we've", 'were', "weren't", 'what', "what's", 'when', "when's", 'where', "where's", 'which', 'while', 'who', "who's", 'whom', 'why', "why's", 'with', "won't", 'would', "wouldn't", 'you', "you'd", "you'll", "you're", "you've", 'your', 'yours', 'yourself', 'yourselves'])
+ALL_STOP_WORDS = NLTK_STOP_WORDS.union(MORE_STOP_WORDS)
+
+TO_REMOVE_STOP_WORDS = set(["where", "what", "who", "how", "when", "which", "why"])
+ALL_STOP_WORDS = ALL_STOP_WORDS.difference(TO_REMOVE_STOP_WORDS)
 
 # For spliting sentance into words , using general split
 # converts to lower case as well
@@ -19,17 +30,14 @@ def split_tokenize(X):
 #function to remove stop words
 def remove_stop(word_list):
     # for removing stop words from dictionary list
-    list_without_stop = [word for word in word_list if not word in STOP_WORDS]
+    list_without_stop = [word for word in word_list if not word in ALL_STOP_WORDS]
     return list_without_stop
 
 #function to remove punctuations
 def remove_punc(word_list):
     # initializing punctuations string  
-    punc = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
-    punc_list = list(punc)
-    list_without_punc = [word for word in word_list if not word in punc_list]
+    list_without_punc = [re.sub(r'[{}]+'.format(punctuation), '', word) for word in word_list]
     return list_without_punc
-
 
 # Wraper function that uses all functionality above on a sentence 
 def process_sentence(sentence):
@@ -53,14 +61,20 @@ def create_vocab(data):
 
 def create_embeddings(vocab):
     embeddings = {}
+    with open("data/glove.txt", 'r') as fp:
+        line = fp.readline()
+        while line:
+            split_line = line.split()
+            embeddings[split_line[0]] = np.array([float(x) for x in split_line[1:]])
+            line = fp.readline()
     vocab_embeddings = []
     for word in vocab:
-        embed = embeddings.get(word)
-        if embed:
+        embed = embeddings.get(word, np.array([]))
+        if embed.size > 0:
             vocab_embeddings.append(embed)
         else:
-            vocab_embeddings.append(embeddings['UNK'])
-    vocab_embeddings.append(embeddings['UNK'])
+            vocab_embeddings.append(embeddings['unk'])
+    vocab_embeddings.append(embeddings['unk'])
     return vocab_embeddings
 
 def create_sentence_representation(sentences, vocab, vocab_embeddings):
@@ -78,9 +92,9 @@ def create_labels_index_representation(labels):
     label_indices = {}
     label_representation = []
     count = 0
-    for label in label:
+    for label in labels:
         if label not in label_indices:
-            label_indices[token] = count
+            label_indices[label] = count
             count += 1
     for label in labels:
         label_representation.append(label_indices[label])
@@ -98,7 +112,7 @@ def preprocess_pipeline(file_path: str):
     label_representation = []
 
     # open file (containing sentences) and read line by line
-    with open(file_path) as fp:
+    with open(file_path, "r") as fp:
         # read first line
         line = fp.readline()
 
@@ -116,7 +130,7 @@ def preprocess_pipeline(file_path: str):
             curr_sentence = remove_punc(curr_sentence)
 
             # add sentence
-            sentences.append(curr_sentence)
+            sentences.append(' '.join(curr_sentence))
 
             # read next line
             line = fp.readline()
@@ -135,3 +149,7 @@ def preprocess_pipeline(file_path: str):
 
     return labels, sentences, vocabulary, vocabulary_embed, sentence_representation, label_index, label_representation
 
+
+labels, sentences, vocabulary, vocabulary_embed, sentence_representation, label_index, label_representation = preprocess_pipeline("res/train_5500.label")
+
+print(sentences)
